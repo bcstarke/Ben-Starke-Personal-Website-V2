914dd2d6e07a46502e088bed183438ec56fa3879
Ben Starke <bstarke0225@gmail.com>
2020-11-11 22:01:41 -0500

    added some work experience stuff and changed container to container fluid

--- src/components/resumePage/ResumePage.js
+++ src/components/resumePage/ResumePage.js
@@ -7,67 +7,65 @@ import './ResumePage.css'
 export default class ResumePage extends Component {
     render() {
         return (
-            <div className="ResumePage">
-                <div className="section-content">
-                    <div className="work-experience-box container-fluid">
-                        <div className="row header">
-                            <h2>Work Experience</h2>
-                            <hr className="top-hr" color="black"/>
-                        </div>
+            <div className="resume-page">
+                <div className="work-experience-container">
+                    <div className="container-fluid">
+                        <div className="section-header">
+                            <div className="row header">
+                                <h2>Work Experience</h2>
+                            </div>
+                            <div className="row header">
+                                <h5>Software Engineering Co-op</h5>
+                                <div className="container-fluid">
+                                    <div className="degree-field">
+                                        <i>Oracle Health Sciences
+                                        </i>
+                                    </div>
+                                </div>
+                            </div>
 
-                        <section>
-                            <div className="row col-12">
-                                {/*<div className="col-2"></div>*/}
-                                <div className="long oracle-name col-7">
-                                    <h4>Software Engineering Co-op</h4>
-                                </div>
-                                <div className="oracle-image col-3">
-                                    {/*<img*/}
-                                    {/*    className="oracle-logo"*/}
-                                    {/*    // src={oraclelogo}*/}
-                                    {/*    alt="oracle-logo"*/}
-                                    {/*/>*/}
-                                </div>
-                            </div>
-                        </section>
-                        <section>
-                            <div className="row">
-                                <div className="envision-image col-3">
-                                    {/*<img*/}
-                                    {/*    className="envision-logo"*/}
-                                    {/*    //src={envisionLogo}*/}
-                                    {/*    alt="envision-logo"*/}
-                                    {/*/>*/}
-                                </div>
-                                <div className="long envision-name">
-                                    <h4>Clinical Information Manager</h4>
-                                </div>
-                            </div>
-                            <div className="long envision-title">
-                                <div className="row">
-                                    <i>Envision Physician Services | Morristown, NJ | December 2015
-                                        - December 2018</i>
-                                </div>
-                            </div>
-                            <div className="row col-13 envision-job-description">
-                                <ul>
-                                    <li>scribe stuff 1</li>
-                                    <li>scribe stuff 2</li>
-                                </ul>
-                            </div>
-                        </section>
+                        {/*<section>*/}
+                        {/*    <div className="row col-12">*/}
+                        {/*        <div className="long oracle-name col-7">*/}
+                        {/*            <h4>Software Engineering Co-op</h4>*/}
+                        {/*        </div>*/}
+                        {/*        <div className="oracle-image col-3">*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*</section>*/}
+                        {/*<section>*/}
+                        {/*    <div className="row">*/}
+                        {/*        <div className="envision-image col-3">*/}
+                        {/*        </div>*/}
+                        {/*        <div className="long envision-name">*/}
+                        {/*            <h4>Clinical Information Manager</h4>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="long envision-title">*/}
+                        {/*        <div className="row">*/}
+                        {/*            <i>Envision Physician Services | Morristown, NJ | December 2015*/}
+                        {/*                - December 2018</i>*/}
+                        {/*        </div>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="row col-13 envision-job-description">*/}
+                        {/*        <ul>*/}
+                        {/*            <li>scribe stuff 1</li>*/}
+                        {/*            <li>scribe stuff 2</li>*/}
+                        {/*        </ul>*/}
+                        {/*    </div>*/}
+                        {/*</section>*/}
                     </div>
                 </div>
 
                 <div className="education-container">
-                    <div className="container" id="educationBox">
-                        <div className="education-header">
+                    <div className="container-fluid" id="educationBox">
+                        <div className="section-header">
                             <div className="row header">
                                 <h2>Education</h2>
                             </div>
                             <div className="row header">
-                                <h4>Northeastern University</h4>
-                                <div className="container">
+                                <h5>Northeastern University</h5>
+                                <div className="container-fluid">
                                     <div className="degree-field">
                                         <i>Candidate for Masters of Science in Computer Science |
                                             Expected Graduation April 2021
@@ -83,8 +81,8 @@ export default class ResumePage extends Component {
                             <br/>
                             <div className="row header">
                                 <br/>
-                                <h4>Seton Hall University</h4>
-                                <div className="container">
+                                <h5>Seton Hall University</h5>
+                                <div className="container-fluid">
                                     <div className="degree-field">
                                         <i>Bachelors of Science in Biochemistry</i>
                                     </div>
